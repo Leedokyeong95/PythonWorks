ff2f29490820f3a1f4c6e1568bd11e1321386199
leedokyeong <nannayamzz95@naver.com>
2021-02-16 19:41:06 +0900

    2021/02/16 Ch02_4_string 예제 완료

--- Ch02/2_4_String.py
+++ Ch02/2_4_String.py
@@ -25,13 +25,28 @@ print('msg의 7번째 문자 :', msg[6])
 print('msg의 11번째 문자 :', msg[10]) # = print('msg의 11번째 문자 :', msg[10])과 동일
 print('msg의 ?번째 문자 :', msg[-1]) # = print('msg의 11번째 문자 :', msg[10])과 동일
 
-
 #문자열 슬라이드
+str = 'Hello Korea'
 
-
+print('str[0:5] :', str[0:5])
+print('str[6:11] :', str[6:11])
+print('str[:5] :', str[:5])
+print('str[6:] :', str[6:])
 
 #문자열 분리
+people = '김유신|김춘추|장보고|강감찬|이순신'
 
+p1, p2, p3, p4, p5 = people.split('|')
 
+print('p1 :', p1)
+print('p2 :', p2)
+print('p3 :', p3)
+print('p4 :', p4)
+print('p5 :', p5)
 
-#문자열 포멧
+#문자열 포맷  포맷:형식  d:숫자 s:문자
+fstr1 = '%d월 %d일'
+fstr2 = '%d월 %d일 %s요일'
+
+print(fstr1 % (2, 16))
+print(fstr2 % (2, 16, '화'))
