306adb89bd6e446701d82f37304707f5b59ebb60
leedokyeong <nannayamzz95@naver.com>
2021-02-16 19:23:15 +0900

    2021/02/16 Ch01, Ch02 수업예제

---
+++ Ch02/2_4_String.py
@@ -0,0 +1,37 @@
+"""
+날짜 : 2021/02/15
+이름 : 이도경
+내용 : 파이썬 문자열(String) 연산 실습하기
+"""
+
+#문자열 더하기
+str1 = 'Hello'
+str2 = 'Python'
+
+result = str1 + str2
+print('result :', result)
+
+#문자열 곱하기
+name = '홍길동'
+print('name * 3 :', name*3)
+
+#문자열 길이 (문자갯수(띄어쓰기 포함))
+msg = 'Hello World'
+print('msg 길이 :', len(msg))
+
+#문자열 인덱스 (단어 첫번째부터 0,1,2~로 나열됨)
+print('msg의 1번째 문자 :', msg[0])
+print('msg의 7번째 문자 :', msg[6])
+print('msg의 11번째 문자 :', msg[10]) # = print('msg의 11번째 문자 :', msg[10])과 동일
+print('msg의 ?번째 문자 :', msg[-1]) # = print('msg의 11번째 문자 :', msg[10])과 동일
+
+
+#문자열 슬라이드
+
+
+
+#문자열 분리
+
+
+
+#문자열 포멧
