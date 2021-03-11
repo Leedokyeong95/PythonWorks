f69817325c26651aef3093589ef16014ebd2a5f0
leedokyeong <nannayamzz95@naver.com>
2021-03-11 21:28:59 +0900

    2021/03/11 TEST

---
+++ Test/test9.py
@@ -0,0 +1,27 @@
+"""
+날짜 : 2021/03/11
+이름 : 이도경
+내용 : 파이썬 연습문제 9. 클래스
+"""
+
+class King:
+
+    def __init__(self, name='태조', year=1392):
+        self.name = name
+        self.year = year
+
+    def show(self):
+        print('-----------------')
+        print('name :', self.name)
+        print('year :', self.year)
+
+
+if __name__ == '__main__':
+
+    king1 = king()
+    king2 = king('태종')
+    king3 = king('세종대왕', 1418)
+
+    king1.show()
+    king2.show()
+    king3.show()
